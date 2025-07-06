bc166d85ad8ef1ece4f46a943e0d1eec8db3fcfb
Nakultt <thunderwolf2657@gmail.com>
2025-07-06 13:21:59 +0530

    made changes

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
+const errorHandler = require("./middleware/errorhandler");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI);;
+console.log("MONGO_URI:", process.env.MONGO_URI);
 
-// Middleware to parse JSON
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected Successfully!");
+})
+.catch((err) => {
+    console.error("MongoDB Connection Error:", err);
+});
+
 app.use(express.json());
-
-// Task routes
 app.use("/api/tasks", require("./routes/taskRoutes"));
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
